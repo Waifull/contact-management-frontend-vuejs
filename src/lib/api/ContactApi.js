cf8a47b5a5305dfb9fb6845464566be77c71dd40
Wafiul Achdi <achdiwafiul@gmail.com>
2025-06-12 23:18:59 +0700

    feat(contact-list): add contact list page with pagination

--- src/lib/api/ContactApi.js
+++ src/lib/api/ContactApi.js
@@ -14,3 +14,19 @@ export const contactCreate = async (token, {first_name, last_name, email, phone}
         })
     })
 }
+
+export const contactList = async (token, {name, email, phone, page}) => {
+    const url = new URL(`${import.meta.env.VITE_API_PATH}/contacts`)
+    if (name) url.searchParams.append('name', name)
+    if (email) url.searchParams.append('email', email)
+    if (phone) url.searchParams.append('phone', phone)
+    if (page) url.searchParams.append('page', page)
+    
+    return await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': token
+        }
+    })
+}
